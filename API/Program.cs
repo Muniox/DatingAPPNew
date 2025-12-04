ebe612716f32047dcdb405507ce342faf4851843
Paweł Bartoszewski <pawel.bartoszewski@lge.com>
2025-12-04 15:57:00 +0100

    Implement initial database setup with Entity Framework Core and add Members API controller

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<API.Data.AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
