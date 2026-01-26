0269f404e26dbf1f13069cf9a1853a2ec342925c
Paweł Bartoszewski <pawel.bartoszewski@lge.com>
2026-01-26 12:45:11 +0100

    Creating an interceptor for the token

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,13 +3,13 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { errorInterceptor } from '../core/interceptors/error-interceptor';
+import { errorInterceptor, jwtInterceptor } from '../core/interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withViewTransitions()),
-    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])),
   ]
 };
