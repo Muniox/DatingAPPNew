1edeb074780b10dba34cb9424c9704f8ba17f174
Paweł Bartoszewski <pawel.bartoszewski@lge.com>
2026-01-28 12:11:39 +0100

    Adding can deactivate guard

--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnDestroy, OnInit, signal, ViewChild} from '@angular/core';
+import {Component, HostListener, inject, OnDestroy, OnInit, signal, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {EditableMember, Member} from '../../../types';
 import {DatePipe} from '@angular/common';
@@ -16,6 +16,11 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class MemberProfile implements OnInit, OnDestroy {
   @ViewChild('editForm') editForm?: NgForm;
+  @HostListener('window:beforeunload', ['$event']) notify($event:BeforeUnloadEvent) {
+    if (this.editForm?.dirty) {
+      $event.preventDefault();
+    }
+  }
   
   protected memberService = inject(MemberService)
   private toast = inject(ToastService);
