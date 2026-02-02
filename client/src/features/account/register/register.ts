cfcf97e9349b4a04657d6a416e7131ba30a762b7
Muniox <muniox20@gmail.com>
2026-02-02 22:55:14 +0100

    Introduction to reactive forms

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,28 +1,42 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {Component, inject, input, OnInit, output} from '@angular/core';
 import { RegisterCreds, User } from '../../../types';
 import { AccountService } from '../../../core/services';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {
+export class Register implements OnInit{
   private accountService = inject(AccountService)
+  private fb = inject(FormBuilder);
+
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds
+  protected registerForm : FormGroup = this.fb.group({
+    email: [],
+    displayName: [],
+    password: [],
+    confirmPassword: []
+  });
+
+  ngOnInit(): void {
+      throw new Error("Method not implemented.");
+  }
 
 
   register() {
-    this.accountService.register(this.creds).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => console.log(error)
-    })
+    console.log(this.registerForm.value)
+    // this.accountService.register(this.creds).subscribe({
+    //   next: response => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: error => console.log(error)
+    // })
   }
 
   cancel() {
