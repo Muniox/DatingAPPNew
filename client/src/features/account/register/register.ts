9ad1be8fe5dbb06c63ed8e7837a090dad1864451
Paweł Bartoszewski <pawel.bartoszewski@lge.com>
2026-02-03 08:59:35 +0100

    Adding custom calidation

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,7 +1,7 @@
 import {Component, inject, input, OnInit, output} from '@angular/core';
 import { RegisterCreds, User } from '../../../types';
 import { AccountService } from '../../../core/services';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import {JsonPipe} from '@angular/common';
 
 @Component({
@@ -10,18 +10,35 @@ import {JsonPipe} from '@angular/common';
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {
+export class Register implements OnInit {
   private accountService = inject(AccountService)
   private fb = inject(FormBuilder);
 
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds
-  protected registerForm : FormGroup = this.fb.group({
+  protected registerForm: FormGroup = this.fb.group({
     email: ['johndoe@test.com', [Validators.required, Validators.email]],
     displayName: ['', Validators.required],
     password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
-    confirmPassword: ['', Validators.required]
+    confirmPassword: ['', [Validators.required, this.matchValues('password')]]
   });
+
+  ngOnInit() {
+    this.registerForm.controls['password'].valueChanges.subscribe(() => {
+      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
+    });
+  }
+
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const parent = control.parent
+      if (!parent) return null;
+      
+      const matchValue = parent.get(matchTo)?.value;
+      return control.value === matchValue ? null : {passwordMismatch: true}
+    }
+  }
 
 
   register() {
