97a47c397662f855ef7cf0211f665433494e5293
Paweł Bartoszewski <pawel.bartoszewski@lge.com>
2026-02-09 16:08:16 +0100

    Adding the lists component functionality

--- client/src/features/lists/lists.ts
+++ client/src/features/lists/lists.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { LikesService } from '../../core/services/likes-service';
+import { Member } from '../../types';
+import { MemberCard } from "../member-card/member-card";
 
 @Component({
   selector: 'app-lists',
-  imports: [],
+  imports: [MemberCard],
   templateUrl: './lists.html',
   styleUrl: './lists.css',
 })
-export class Lists {
+export class Lists implements OnInit {
+  private likeService = inject(LikesService);
+  protected members = signal<Member[]>([]);
+  protected predicate = 'liked';
 
+  tabs = [
+    {label: 'liked', value: 'liked'},
+    {label: 'liked me', value: 'likedBy'},
+    {label: 'Mutual', value: 'mutual'}
+  ]
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  setPredicate(predicate: string) {
+    if (this.predicate !== predicate) {
+      this.predicate = predicate;
+      this.loadLikes();
+    }
+  }
+
+  loadLikes() {
+    this.likeService.getLikes(this.predicate).subscribe({
+      next: members => this.members.set(members),
+    })
+  }
 }
