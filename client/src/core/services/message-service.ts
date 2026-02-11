9c9cc82aceac508aad20aa7340ebe5c9be715dcb
Paweł Bartoszewski <pawel.bartoszewski@lge.com>
2026-02-11 11:05:45 +0100

    Setting up the angular app for messaging

---
+++ client/src/core/services/message-service.ts
@@ -0,0 +1,24 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { PaginatedResult } from '../../types';
+import { Message } from '../../types/message';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  private baseUrl = environment.baseUrl;
+  private http = inject(HttpClient);
+
+  getMessage(container: string, pageNumber: number, pageSize: number) {
+    let params = new HttpParams();
+
+    params = params
+      .append('pageNumber', pageNumber)
+      .append('pageSize', pageSize)
+      .append('container', container)
+
+      return this.http.get<PaginatedResult<Message>>(this.baseUrl + 'messages', {params})
+  }
+}
