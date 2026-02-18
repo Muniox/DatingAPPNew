b6b607b1222bd672e036ce388ac8a0ac23a7abe0
Paweł Bartoszewski <pawel.bartoszewski@lge.com>
2026-02-18 14:36:46 +0100

    Adding an admin guard

--- API/Program.cs
+++ API/Program.cs
@@ -64,6 +64,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddAuthorizationBuilder()
     .AddPolicy("RequiredAdminRole", policy => policy.RequireRole("Admin"))
     .AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -13,6 +13,7 @@ import { MemberMessages } from '../features/members/member-messages/member-messa
 import { memberResolver } from '../features/members/member-resolver';
 import { preventUnsvedChangesGuard } from '../core/guards/prevent-unsved-changes-guard';
 import { Admin } from '../features/admin/admin';
+import { adminGuard } from '../core/guards/admin-guard';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -36,7 +37,7 @@ export const routes: Routes = [
       },
       { path: 'lists', component: Lists },
       { path: 'messages', component: Messages },
-      { path: 'admin', component: Admin },
+      { path: 'admin', component: Admin, canActivate: [adminGuard] },
     ],
   },
   { path: 'errors', component: TestErrors },
