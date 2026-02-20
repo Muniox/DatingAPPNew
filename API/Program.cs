8d3328ea030fb837d069f7079a19b2265f252d8a
Paweł Bartoszewski <pawel.bartoszewski@lge.com>
2026-02-20 08:29:11 +0100

    Configuring authentication to SignalR

--- API/Program.cs
+++ API/Program.cs
@@ -61,6 +61,22 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateIssuer = false,
             ValidateAudience = false
         };
+
+        // podpięcie pod huba websocket
+        options.Events = new JwtBearerEvents()
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+                var path = context.HttpContext.Request.Path;
+                if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+                {
+                    context.Token = accessToken;
+                }
+
+                return Task.CompletedTask;
+            }
+        };
     });
 
 builder.Services.AddAuthorizationBuilder()
